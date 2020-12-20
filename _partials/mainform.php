619064b100fc6dbec63abfa7ed86519f66cc7289
matusrebros <rebros.matus@gmail.com>
2020-12-21 00:21:24 +0100

    homepage update

---
+++ _partials/mainform.php
@@ -0,0 +1,29 @@
+<section class="home-form">
+			<div class="wrapper">
+				<img class="img-top-form" src="./assets/img/home-form.svg">
+				<div class="container">
+					<div class="content">
+						<h3>Máte záujem o stretnutie?</h3>
+						<p>V prípade záujmu nás neváhajte kontaktovať prostredníctvom formulára alebo telefonicky. Náš predajca si s Vami dohodne osobné stretnutie.</p>
+						<a href="#">
+							<img src="./assets/img/phone.svg">
+							+421 911 123 456
+						</a>
+						<a href="#">
+							<img src="./assets/img/mail.svg">
+							info@rozprávkovotatry.sk
+						</a>
+					</div>
+					<form>
+						<input type="text" placeholder="Meno a Priezvisko">
+						<input type="email" placeholder="Email">
+						<input type="number" placeholder="Tel. č.">
+						<label>
+							<input type="checkbox">
+							<span>Súhlasím so spracovaním <a href="#"> osobných údajov</a></spa>
+						</label>
+						<button type="submit">Odoslať</button>
+					</form>
+				</div>
+			</div class="wrapper">
+		</section>
---
+++ index.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Rozpravkovo</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;600;700&family=Pacifico&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+	<link rel="stylesheet" href="./assets/css/style.min.css">
+</head>
+<body>
+
+	<header>
+		<a href="/">
+			<img src="./assets/img/main-logo.svg">
+		</a>
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Ponuka Nehnuteľností</a>
+				</li>
+				<li>
+					<a class="speceial-one" href="#">Prenájom Naveky</a>
+					<span>Time Sharing</span>
+				</li>
+				<li>
+					<a href="#">Aktivity</a>
+				</li>
+				<li>
+					<a href="#">O projekte</a>
+				</li>
+				<li>
+					<a href="#">Novinky</a>
+				</li>
+				<li>
+					<a href="#">Galéria</a>
+				</li>
+				<li>
+					<a href="#">Kontakt</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="language-selector">
+			<span class="selected-language">SK</span>
+			<div class="another-laguages">
+				<span>EN</span>
+				<span>PL</span>
+			</div>
+		</div>
+	</header>
+
+	<main>
+		<section class="hero-section">
+			<div class="container">
+				<div class="content">
+					<img class="hero-logo" src="./assets/img/hero-logo.svg">
+					<div class="main-headline">
+						<img src="./assets/img/main-heading-arrow-left.svg">
+						<h1>Aj rozprávka sa môže stať skutočnosťou.</h1>
+						<img src="./assets/img/main-heading-arrow-right.svg">
+					</div>
+					<p>Rozprávkové bývanie pri slonovi v Tatranskej Lomnici</p>
+					<div class="hero-ctas">
+						<a class="orange-cta" href="#">Nehnuteľnosti</a>
+						<a href="#">Chaty Rozprávkovo</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="home-about">
+			<div class="container">
+				<h2>O Projekte</h2>
+				<img class="divider" src="./assets/img/home-about-element.svg">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<div class="advantages">
+					<div class="advantage">
+						<img src="./assets/img/advantage1.svg">
+						<span>Množstvo aktivít</span>
+					</div>
+					<div class="advantage">
+						<img src="./assets/img/advantage2.svg">
+						<span>Skvelé prostredie pre deti</span>
+					</div>
+					<div class="advantage">
+						<img src="./assets/img/advantage3.svg">
+						<span>Garážové parkovanie</span>
+					</div>
+					<div class="advantage">
+						<img src="./assets/img/advantage4.svg">
+						<span>Rozprávková príroda</span>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="home-location">
+			<div class="container">
+				<img src="./assets/img/home-location.png">
+				<div class="content">
+					<img src="./assets/img/location-element.svg">
+					<h3>Tatranská Lomnica</h3>
+					<strong>Eurocamp 14599</strong>
+					<span>059 60 Vysoké Tatry</span>
+					<a href="#">Google Mapy</a>
+				</div>
+			</div>
+		</section>
+		<section class="home-counters">
+			<div class="container">
+				<div class="counters">
+					<div class="counter">
+						<strong>30</strong>
+						<span>Apartmánov</span>
+					</div>
+					<div class="counter">
+						<strong class="special">2</strong>
+						<span>Dvoj-apart-domy</span>
+					</div>
+					<div class="counter">
+						<strong>3</strong>
+						<span>Apart-domy</span>
+					</div>
+					<div class="counter">
+						<strong>3</strong>
+						<span>Chaty</span>
+					</div>
+				</div>
+				<a href="#">Viac</a>
+			</div>
+		</section>
+		<section class="home-kupa-prenajom">
+			<div class="container">
+				<div class="element kupa-element">
+					<h3>Kúpa</h3>
+					<img src="./assets/img/white-element-kupa-prenajom.svg">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<a href="#">Viac</a>
+				</div>
+				<div class="element prenajom-element">
+					<h3>Prenájom Naveky</h3>
+					<img src="./assets/img/white-element-kupa-prenajom.svg">
+					<p>Načo kupovať za státisíce, keď si môžeš prenajať naveky za tisíce...</p>
+					<a href="#">Viac</a>
+				</div>
+			</div>
+		</section>
+		<section class="home-aktivity">
+			<div class="container">
+				<div class="content">
+					<h2>Aktivity</h2>
+					<img class="divider" src="./assets/img/home-about-element.svg">
+					<strong>Okolie nabité Rozprávkovými aktivitami</strong>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec massa sapien faucibus et molestie ac. Semper quis lectus nulla at volutpat diam.</p>
+					<a href="#">Viac</a>
+				</div>
+			</div>
+		</section>
+
+		<section class="home-long-bg">
+			<div class="container">
+				<section class="home-novinky">
+					<div class="container">
+						<h2>Novinky</h2>
+						<img src="./assets/img/partneri-pattern.svg">
+						<div class="articles">
+							<article class="green">
+								<strong>V areáli Rozprávkova bude stáť detské ihrisko</strong>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<img src="./assets/img/home-article-element.svg">
+							</article>
+							<article class="blue">
+								<strong>V areáli Rozprávkova bude stáť detské ihrisko</strong>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<img src="./assets/img/home-article-element.svg">
+							</article>
+							<article class="orange">
+								<strong>V areáli Rozprávkova bude stáť detské ihrisko</strong>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<img src="./assets/img/home-article-element.svg">
+							</article>
+							<article class="green">
+								<strong>V areáli Rozprávkova bude stáť detské ihrisko</strong>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<img src="./assets/img/home-article-element.svg">
+							</article>
+							<article class="blue">
+								<strong>V areáli Rozprávkova bude stáť detské ihrisko</strong>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<img src="./assets/img/home-article-element.svg">
+							</article>
+						</div>
+					</div>
+				</section>
+				<section class="home-galeria">
+					<div class="container">
+						<h2>Galéria</h2>
+						<img src="./assets/img/home-galery-element.svg">
+					</div>
+				</section>
+			</div>
+		</section>
+
+		<?php include '_partials/mainform.php'; ?>
+		<section class="home-realizovane">
+			<div class="container">
+				<h3>Realizovali sme</h3>
+				<img class="divider" src="./assets/img/home-about-element.svg">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec massa sapien faucibus et molestie ac. Semper quis lectus nulla at volutpat diam.</p>
+				<div class="content">
+					<img src="./assets/img/realizovane-img.jpg">
+					<div class="chaty-viac">
+						<span>Chaty</span>
+						<span>Rozprávkovo</span>
+						<a href="#">Viac</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="home-partneri">
+			<div class="container">
+				<h3>Partneri</h3>
+				<img class="divider" src="./assets/img/partneri-pattern.svg">
+				<div class="logos">
+					<img src="./assets/img/sample-logo-company.png">
+					<img src="./assets/img/sample-logo-company.png">
+					<img src="./assets/img/sample-logo-company.png">
+					<img src="./assets/img/sample-logo-company.png">
+					<img src="./assets/img/sample-logo-company.png">
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<footer>
+	<a href="/">
+			<img class="top-logo" src="./assets/img/main-logo.svg">
+		</a>
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Ponuka Nehnuteľností</a>
+				</li>
+				<li>
+					<a class="speceial-one" href="#">Prenájom Naveky</a>
+					<span>Time Sharing</span>
+				</li>
+				<li>
+					<a href="#">Aktivity</a>
+				</li>
+				<li>
+					<a href="#">O projekte</a>
+				</li>
+				<li>
+					<a href="#">Novinky</a>
+				</li>
+				<li>
+					<a href="#">Galéria</a>
+				</li>
+				<li>
+					<a href="#">Kontakt</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="contacts">
+			<div class="contact">
+				<img src="./assets/img/footer-phone.svg">
+				<a href="#">+ 421 911 123 456</a>
+			</div>
+			<div class="contact">
+				<img src="./assets/img/footer-mail.svg">
+				<a href="#">info@rozpravkovotatry.sk</a>
+			</div>
+			<div class="contact">
+				<img src="./assets/img/footer-address.svg">
+				<a href="#">Adresa 10, Tatranská Lomnica</a>
+			</div>
+			<div class="contact">
+				<img src="./assets/img/footer-fb.svg">
+				<a href="#">Facebook</a>
+			</div>
+		</div>
+	</footer>
+
+	<div class="copyright">
+		<div class="container">
+			<span>Designed by</span>
+			<a href="#">
+				<img src="./assets/img/brandly-logo.svg">
+			</a>
+			<span>built by</span>
+			<a href="#">
+				<img src="./assets/img/dp-logo.svg">
+			</a>
+		</div>
+	</div>
+
+</body>
+</html>
